0ce9ca322479654250132f691e44f192cd7213de
Ziva Skof <ziva.skof@kamino.si>
2023-04-26 11:23:08 +0200

    Update tINIT.m
    adapt code for multiple models

--- tINIT.m
+++ tINIT.m
@@ -3,30 +3,36 @@ initCobraToolbox(false)
 changeCobraSolver('gurobi','all');
 setRavenSolver('gurobi');
 
-gtex_data = readtable("dataGSE39791_H.xlsx");
+gtex_data = readtable("dataGSE39791_H_C.xlsx");
 %gtex_data(1:5,1:5)
-
-% extract the tissue and gene names
-data_struct.tissues = gtex_data.Properties.VariableNames(2)'  
-data_struct.genes = gtex_data.Symbol;  
-data_struct.levels = table2array(gtex_data(:, 2:end));
-
-data_struct.threshold = 1;
-%data_struct;
 
 %Load model and convert model
 load('/Users/zivaskof/Documents/MATLAB/mag/Human-GEM-1.14.0/model/Human-GEM.mat');
 ihuman = addBoundaryMets(ihuman);
 essentialTasks_H=parseTaskList('/Users/zivaskof/Documents/MATLAB/mag/Human-GEM-1.14.0/data/metabolicTasks/metabolicTasks_Essential.txt');
+model_Healthy=ravenCobraWrapper(ihuman);
 
+num_columns = size(gtex_data, 2);
 
+for col = 2:num_columns
 
-% see what the other inputs mean by typing "help checkTasks"
-%checkTasks(ihuman, [], true, false, false, essentialTasks_H);
+    nameAr = gtex_data.Properties.VariableNames(col);
+    name = char(nameAr)
 
-refModel = ihuman;          
-tissue = 'KM_001N';           
-arrayData = data_struct;   
-taskStructure = essentialTasks_H; 
+    % extract the tissue and gene names
+    data_struct.tissues = gtex_data.Properties.VariableNames(col)'; % sample (tissue) names
+    data_struct.genes = gtex_data.Gene_ID;  % gene names
+    data_struct.levels = table2array(gtex_data(:, col));  % gene TPM values
+    a = table2array(gtex_data(:, col));
+    data_struct.threshold = prctile(a, 75);
 
-liverGEM = getINITModel2(refModel, tissue, [], [], arrayData, [], true, [], true, true, taskStructure, [], []);
+    data_struct
+    
+    % see what the other inputs mean by typing "help checkTasks"
+    checkTasks(ihuman, [], true, false, false, essentialTasks_H);
+    
+    
+    liver_tINIT_H = getINITModel2(ihuman, name, [], [], data_struct, [], true, [], true, true, essentialTasks_H, [], []);
+
+    save(sprintf('%s.mat', name), 'liver_tINIT_H');
+end
